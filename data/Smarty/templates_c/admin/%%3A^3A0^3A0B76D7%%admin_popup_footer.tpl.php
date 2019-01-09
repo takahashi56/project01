80ca47d49580ad82f54310c92aff8be083502d2b
takahasi <webexpert007@yahoo.co.jp>
2019-01-09 11:15:51 +0700

    fixed untracked files

--- data/Smarty/templates_c/admin/%%3A^3A0^3A0B76D7%%admin_popup_footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2018-02-23 14:59:09
-         compiled from /home/oomaekunccb/mallento.com/public_html/stmingo-demo//data/Smarty/templates/admin/admin_popup_footer.tpl */ ?>
-
-</div>
-</body>
-</html>
--- data/Smarty/templates_c/admin/%%D5^D56^D563EA35%%plg_Coupon_coupon_complete.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2017-09-23 15:30:33
-         compiled from contents/plg_Coupon_coupon_complete.tpl */ ?>
-<div id="admin-contents" class="contents-main"><center>
-    <div class="message">
-        <span>クーポンの登録・編集が完了しました </span>
-    </div>
-    <br />
-    <a class="btn-action" href="./plg_Coupon_coupon.php"><span class="btn-prev">クーポン一覧に戻る</span></a>
-</center></div>
